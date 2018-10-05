adb950775795fa55af946ae99a511e93d925c157
Jinxed <toxep@mail.ru>
2018-10-05 17:56:44 +0300

    rss xml parsed to json

--- routes/results.js
+++ routes/results.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const {ensureAuthenticated} = require('../helpers/auth');
 const axios = require('axios');
+const convert = require('xml-js');
 
 
 
@@ -11,7 +12,25 @@ router.get('/', (req, res)=>{
         method:'get',
         url : 'http://zakupki.gov.ru/epz/order/quicksearch/rss',
         params: {
-            searchString: 'масло'
+            searchString: 'отбойник',
+            morphology:'on',
+            openMode:'USE_DEFAULT_PARAMS',
+            pageNumber:1,
+            sortDirection:false,
+            recordsPerPage: '_10',
+            showLotsInfoHidden:false,
+            fz44:'on',
+            fz223:'on',
+            ppRf615:'on',
+            af:'on',
+            ca:'on',
+            pc:'on',
+            pa:'on',
+            currencyIdGeneral:'-1',
+            publishDateFrom:'01.10.2018',
+            publishDateTo:'05.10.2018',
+            regionDeleted:'false&oktmoIdsWithNested=on',
+            sortBy:'UPDATE_DATE'
         },
         headers: {
             'accept': 'application/json',
@@ -19,9 +38,10 @@ router.get('/', (req, res)=>{
         },
     })
         .then((response)=>{
-            console.log(response)
+            const resp = convert.xml2json(response.data, {compact: false, spaces: 5});
+            console.log(resp)
             res.render('results/index',{
-                data: response.data
+                data: resp
             })
         })
         .catch((err)=>{
